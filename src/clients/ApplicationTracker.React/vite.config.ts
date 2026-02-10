21070c2a41cc0bc1af366f9857ca74fec1597b16
tlhuynh <tuanhuynh97@gmail.com>
2026-02-10 13:47:15 -0600

    Removed boilerplate code from vite

--- src/clients/ApplicationTracker.React/vite.config.ts
+++ src/clients/ApplicationTracker.React/vite.config.ts
@@ -1,4 +1,4 @@
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
@@ -8,9 +8,5 @@ export default defineConfig({
 		globals: true,
 		environment: 'jsdom',
 		setupFiles: './src/test/setup.ts',
-		// might be able to remove this when cleaning up boostrap code
-		alias: {
-			'/vite.svg': './public/vite.svg',
-		},
 	},
 });
