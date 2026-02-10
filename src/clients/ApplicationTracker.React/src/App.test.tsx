07a96f005a31ad17ba8690d3946646351ecd33de
tlhuynh <tuanhuynh97@gmail.com>
2026-02-10 13:47:15 -0600

    Removed boilerplate code from vite

--- src/clients/ApplicationTracker.React/src/App.test.tsx
+++ src/clients/ApplicationTracker.React/src/App.test.tsx
@@ -5,6 +5,6 @@ import { App } from './App';
 describe('App', () => {
 	it('renders the heading', () => {
 		render(<App />);
-		expect(screen.getByText('Vite + React')).toBeInTheDocument();
+		expect(screen.getByText('Application Tracker')).toBeInTheDocument();
 	});
 });
