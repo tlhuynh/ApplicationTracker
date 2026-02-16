7897ddcd6e9ba8f461122a17da2f7bfc7d187b9d
tlhuynh <tuanhuynh97@gmail.com>
2026-02-15 22:32:33 -0600

    Configured and registered auth service + create controller for auth

--- src/backend/ApplicationTracker.Api/Program.cs
+++ src/backend/ApplicationTracker.Api/Program.cs
@@ -1,6 +1,11 @@
+using System.Text;
 using ApplicationTracker.Api.Services;
 using ApplicationTracker.Core.Interfaces.Services;
 using ApplicationTracker.Infrastructure;
+using ApplicationTracker.Infrastructure.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
 using Scalar.AspNetCore;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
@@ -17,9 +22,47 @@ builder.Services.AddCors(options => {
 
 builder.Services.AddInfrastructure(builder.Configuration);
 
+// Identity and JWT configuration
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options => {
+		// Tighten these rules when use for production
+		options.Password.RequiredLength = 6;
+		options.Password.RequireDigit = false;
+		options.Password.RequireNonAlphanumeric = false;
+		options.Password.RequireUppercase = false;
+		options.Password.RequireLowercase = false;
+		options.User.RequireUniqueEmail = true;
+	})
+	.AddEntityFrameworkStores<ApplicationDbContext>()
+	.AddDefaultTokenProviders();
+
+// NOTES: about this key, can come up ourselves or generate it randomly for better secure
+// For dev, we can generate once and keep forever. For Prod, ideally it should be changed periodically. When it happened
+// all previous keys will be invalid and required re-authenticate.
+string jwtKey = builder.Configuration["Jwt:Key"]
+                ?? throw new InvalidOperationException("JWT key is not configured.");
+
+builder.Services.AddAuthentication(options => {
+		options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+		options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+	})
+	.AddJwtBearer(options => {
+		options.TokenValidationParameters = new TokenValidationParameters {
+			ValidateIssuer = true,
+			ValidateAudience = true,
+			ValidateLifetime = true,
+			ValidateIssuerSigningKey = true,
+			ValidIssuer = builder.Configuration["Jwt:Issuer"],
+			ValidAudience = builder.Configuration["Jwt:Audience"],
+			IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
+		};
+	});
+
+builder.Services.AddAuthorization();
+
 // Register DI for services
 builder.Services.AddScoped<IApplicationRecordService, ApplicationRecordService>();
 builder.Services.AddScoped<IExcelImportService, ExcelImportService>();
+builder.Services.AddScoped<ITokenService, TokenService>();
 
 WebApplication app = builder.Build();
 
@@ -30,6 +73,8 @@ if (app.Environment.IsDevelopment()) {
 
 app.UseCors();
 app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
