84f2a4110f262f8fe5f67e3fe5514f419378a428
tlhuynh <tuanhuynh97@gmail.com>
2026-02-15 20:22:20 -0600

    Added new DTOs and new setting

---
+++ src/shared/ApplicationTracker.Shared/DTOs/LoginRequest.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApplicationTracker.Shared.DTOs;
+
+/// <summary>
+/// Request model for user login.
+/// </summary>
+public class LoginRequest {
+	/// <summary>
+	/// Gets or sets the email address.
+	/// </summary>
+	[Required]
+	[EmailAddress]
+	public string Email { get; set; } = string.Empty;
+
+	/// <summary>
+	/// Gets or sets the password.
+	/// </summary>
+	[Required]
+	public string Password { get; set; } = string.Empty;
+}
